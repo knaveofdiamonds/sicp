9ca1f3500682f31f390fe5144e61dd6188bc0409
Roland Swingler <roland.swingler@gmail.com>
2012-02-13 22:39:17 +0000

    Exercise 1.10

--- 1.2.scm
+++ 1.2.scm
@@ -34,7 +34,30 @@
 
 ;; 1.10
 
-;; TODO!
+(define (A x y)
+  (cond ((= y 0) 0)
+        ((= x 0) (* 2 y))
+        ((= y 1) 2)
+        (else (A (- x 1)
+                 (A x (- y 1))))))
+
+(A 1 10)
+; 1024
+
+(A 2 4)
+; 65536
+
+(A 3 3)
+; 65536
+
+; Gives 2y
+(define (fA n) (A 0 n))
+
+; Gives 2^y
+(define (gA n) (A 1 n))
+
+; Gives 2^^y
+(define (hA n) (A 2 n))
 
 ;; 1.11
 
