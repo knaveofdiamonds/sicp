2262e474e5a579ae19da9f06071b3782c24d2ac4
Roland Swingler <roland.swingler@gmail.com>
2012-02-25 14:39:16 +0000

    Exercise 1.15

--- 1.2.scm
+++ 1.2.scm
@@ -92,3 +92,66 @@
 (define (pascal row col)
   (if (or (eq? col 1) (eq? col row)) 1
       (+ (pascal (- row 1) (- col 1)) (pascal (- row 1) col))))
+
+
+;; 1.14
+
+(define (count-change amount)
+  (cc amount 5))
+(define (cc amount kinds-of-coins)
+  (cond ((= amount 0) 1)
+        ((or (< amount 0) (= kinds-of-coins 0)) 0)
+        (else (+ (cc amount
+                     (- kinds-of-coins 1))
+                 (cc (- amount
+                        (first-denomination kinds-of-coins))
+                     kinds-of-coins)))))
+(define (first-denomination kinds-of-coins)
+  (cond ((= kinds-of-coins 1) 1)
+        ((= kinds-of-coins 2) 5)
+        ((= kinds-of-coins 3) 10)
+        ((= kinds-of-coins 4) 25)
+        ((= kinds-of-coins 5) 50)))
+
+;; (cc 11 5)
+;; (+ (cc 11 4) (cc (- 11 50) 5))
+;; (+ (cc 11 4) 0)
+;; (cc 11 4)
+;; (+ (cc 11 3) (cc (- 11 25) 4))
+;; (+ (cc 11 3) 0)
+;; (cc 11 3)
+;; (+ (cc 11 2) (cc 1 3))
+;; (+ (cc 11 2) (+ (cc 1 2) (cc -9 3)))
+;; (+ (cc 11 2) (+ (cc 1 2) 0))
+;; (+ (cc 11 2) (cc 1 2))
+;; (+ (cc 11 2) (+ (cc 1 1) (cc -4 2)))
+;; (+ (cc 11 2) (+ (cc 1 1) 0))
+;; (+ (cc 11 2) (cc 1 1))
+;; (+ (cc 11 2) (+ (cc 1 0) (cc 0 1)))
+;; (+ (cc 11 2) (+ 0 1))
+;; (+ (cc 11 2) 1)
+;; (+ (+ (cc 11 1) (cc 6 2)) 1)
+;; (+ (+ (cc 11 1) (+ (cc 6 1) (cc 1 1))) 1)
+;; (+ (+ (cc 11 1) (+ (cc 6 1) (+ (cc 1 0) (cc 0 1)))) 1)
+;; (+ (+ (cc 11 1) (+ (cc 6 1) (+ 0 1))) 1)
+;; (+ (+ (cc 11 1) (+ (cc 6 1) 1)) 1)
+
+
+;; 1.15
+
+(define (cube x) (* x x x))
+(define (p x) (- (* 3 x) (* 4 (cube x))))
+(define (sine angle)
+   (if (not (> (abs angle) 0.1))
+       angle
+       (p (sine (/ angle 3.0)))))
+
+;; (sine 12.5)
+;; (p (sine 4.17))
+;; (p (p (sine 1.39)))
+;; (p (p (p (sine 0.46))))
+;; (p (p (p (p (sine 0.15)))))
+;; (p (p (p (p (p (sine 0.05))))))
+
+;; p applied 5 times
+;; O(log a) growth, O(log a) space
