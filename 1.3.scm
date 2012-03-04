7812fce5f42dfa44f13668a56796690ee2196285
Roland Swingler <roland.swingler@gmail.com>
2012-03-04 22:11:25 +0000

    1.31, 1.32

--- 1.3.scm
+++ 1.3.scm
@@ -25,3 +25,39 @@
         result
         (iter (next a) (+ (term a) result))))
   (iter a 0))
+
+;; 1.31
+
+(define (identity x) x)
+(define (inc x) (+ x 1))
+
+(define (product1 term a next b)
+  (if (> a b)
+      1
+      (* (term a)
+         (product1 term (next a) next b))))
+
+
+(define (product2 term a next b)
+  (define (iter a result)
+    (if (> a b)
+        result
+        (iter (next a) (* (term a) result))))
+  (iter a 1))
+
+(define (factorial n) (product1 identity 1 inc n))
+
+;; 1.32
+
+(define (accumulate combiner null-value term a next b)
+  (define (iter a result)
+    (if (> a b)
+        result
+        (iter (next a) (combiner (term a) result))))
+  (iter a null-value))
+
+(define (sum2 term a next b)
+  (accumulate + 0 term a next b))
+
+(define (product3 term a next b)
+  (accumulate * 1 term a next b))
