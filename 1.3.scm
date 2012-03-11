f2ba1648924f4fef1c1e9b1b34a286b2481689cd
Roland Swingler <roland.swingler@gmail.com>
2012-03-11 12:55:30 +0000

    1.35

--- 1.3.scm
+++ 1.3.scm
@@ -95,3 +95,20 @@
   (g 2))
 
 ; We'll get an error calling (f f), because eventually we'll be trying to evaluate (2 2) which doesn't make sense.
+
+;; 1.35
+
+(define tolerance 0.00001)
+(define (fixed-point f first-guess)
+  (define (close-enough? v1 v2)
+    (< (abs (- v1 v2)) tolerance))
+  (define (try guess)
+    (let ((next (f guess)))
+      (if (close-enough? guess next)
+          next
+          (try next))))
+  (try first-guess))
+
+(define golden-ratio
+  (fixed-point (lambda (x) (+ 1 (/ 1 x)))
+               1.0))
